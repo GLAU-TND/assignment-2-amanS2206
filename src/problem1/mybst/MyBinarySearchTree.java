689b1ef75d945dde343c9579b00a1a1e978b6ae3
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-03-28 01:58:46 +0530

    create add method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,32 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private int numberOfNodes;
+    private TreeNode root;
+
+    private TreeNode addRecursive(TreeNode currentNode, int data) {
+        if (currentNode == null) {
+            return new TreeNode(data);
+        }
+        if ((Integer) data < (Integer) currentNode.getData()) {
+            currentNode.leftChild = addRecursive(currentNode.getLeftChild(), data);
+        } else if ((Integer) data > (Integer) currentNode.getData()) {
+            currentNode.rightChild = addRecursive(currentNode.getRightChild(), data);
+        }
+        return currentNode;
+    }
+
+
+    public boolean add(int data) {
+        root = addRecursive(root, data);
+        numberOfNodes++;
+        return true;
+    }
+
+
 }
