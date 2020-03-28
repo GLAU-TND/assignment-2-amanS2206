2d1670455abee210a9280e3725146323cdb1f8e9
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-03-28 21:28:54 +0530

    modify main class of problem 4

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+import java.util.Random;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        Random r = new Random();
+        int i = r.nextInt(100);
+        for (int j = 0; j < i; j++) {
+            obj.add(r.nextInt(100));
+        }
+        MyQueue queue = new MyQueue();
+        queue.enqueue(obj.root);
+        queue.display();
+
+    }
 }
