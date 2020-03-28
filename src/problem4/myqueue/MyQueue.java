fdc8ed482b33f0d21fa3f8dc42e15f5687f2681f
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-03-28 18:09:45 +0530

    made MyQueue class

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,67 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem1.node.TreeNode;
+import problem4.node.Node;
+
+import static problem1.mybst.MyBinarySearchTree.arr;
+import static problem1.mybst.MyBinarySearchTree.preOrder;
+
 public class MyQueue {
+
+    private Node rear;
+
+    private Node front;
+    private int size;
+
+
+    public MyQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
+
+    public boolean isEmpty() {
+        boolean response = false;
+        if (size == 0) {
+            response = true;
+        }
+        return response;
+    }
+
+
+    public void enqueue(TreeNode root) {
+        preOrder(root);
+        for (int i = 0; i < arr.size(); i++) {
+            Node node = new Node(arr.get(i));
+            if (front == null) {
+                rear = node;
+                front = node;
+                size++;
+            } else {
+                rear.setNext(node);
+                rear = node;
+                size++;
+            }
+
+        }
+
+    }
+
+    public void display() {
+        Node temp = this.front.getNext();
+        for (int i = 0; i < this.size - 1; i++) {
+            if (i != this.size - 2) {
+                System.out.print(temp.getData() + "->");
+                temp = temp.getNext();
+            } else {
+                System.out.println(temp.getData());
+            }
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
