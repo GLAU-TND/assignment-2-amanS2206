8f2bcd40479e53a634f469a2eab319763b1e8477
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-03-28 13:53:02 +0530

    add offer getnode getrightindex methods in MyPriorityQueue class

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,6 +6,7 @@
  */
 package problem3.myqueue;
 import problem3.node.Node;
+import problem5.student.Student;
 
 public class MyPriorityQueue {
     private Node rear;
@@ -17,4 +18,58 @@ public class MyPriorityQueue {
         rear = null;
         size = 0;
     }
+
+    public boolean offer(Student student) {
+        int index = getRightIndex(student);
+        if (index == 0) {
+            if (front == null) {
+                front = new Node(student);
+                rear = front;
+                size++;
+            } else {
+                Node newNextNode = front;
+                front = new Node(student, newNextNode);
+                size++;
+            }
+
+        } else if (index > 0 && index < size) {
+            Node newNextNode = getNode(index);
+            getNode(index - 1).setNext(new Node(student, newNextNode));
+            size++;
+        } else if (index == size) {
+            Node newPreviousNode = getNode(index - 1);
+            Node newNode = new Node(student);
+            newPreviousNode.setNext(new Node(student));
+            rear = newNode;
+            size++;
+        }
+        return true;
+    }
+
+    private Node getNode(int index) {
+        Node response = this.front;
+        for (int i = 0; i < index; i++) {
+            response = response.getNext();
+        }
+        return response;
+    }
+
+    private int getRightIndex(Student student) {
+        if (this.size == 0) {
+            return 0;
+        } else {
+            for (int i = 0; i < this.size; i++) {
+                Student item = getNode(i).getData();
+                int compare = item.compareTo(student);
+                if (compare >= 0) {
+                    return i;
+                }
+
+            }
+        }
+        return size;
+
+    }
+
+
 }
